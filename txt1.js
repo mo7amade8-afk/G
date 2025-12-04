9488a4500a07e42409923cc0b89e93aeb7488d0b
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 14:26:29 +0100

    Update txt1.js

--- txt1.js
+++ txt1.js
@@ -1,1 +1,12 @@
+export function run(cmd) {
+  if (cmd === "cat") {
+    return {
+      type: "text",
+      content: "Hello from txt1.js"
+    };
+  }
 
+  return { error: "unknown text command" };
+}
+
+export default { run };
