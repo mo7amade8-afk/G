b988bf2a6754c07c8930334c3a89caedf60529d5
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 08:08:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+import "dotenv/config";
+import { Telegraf } from "telegraf";
+import KING_admin from "./KING_admin.js";
 
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// 
+bot.on("message", (ctx) => KING_admin(ctx));
+
+bot.launch();
+console.log("BOT RUNNING...");
