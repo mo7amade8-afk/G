b1019f1d3606a889fd1099a771ef8ef0da00df43
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 14:17:51 +0100

    Update admin_control.js

--- admin_control.js
+++ admin_control.js
@@ -1,1 +1,13 @@
+import image from "./image.js";
+import text from "./text.js";
+import { send } from "./admin_key.js";
 
+export function controlImage(cmd) {
+  send(image.handle(cmd));
+}
+
+export function controlText(cmd) {
+  send(text.handle(cmd));
+}
+
+export default { controlImage, controlText };
