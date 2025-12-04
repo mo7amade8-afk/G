eef3bce51f88c792eb81c258c9ccb0f47dfb82e9
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 08:56:19 +0100

    Update KING_admins.js

--- KING_admins.js
+++ KING_admins.js
@@ -1,1 +1,17 @@
+const server = require("./server");
+const admin_key = require("./admin_key");
 
+function fromServer(msg) {
+  console.log("KING_admins received:", msg);
+  admin_key.fromKING(msg);
+}
+
+function fromKey(msg) {
+  console.log("KING_admins gets from admin_key:", msg);
+  server.fromKING(msg);
+}
+
+module.exports = {
+  fromServer,
+  fromKey
+};
