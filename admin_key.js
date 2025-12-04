7f9f17162f24bb1f756112c78db7f280b15be939
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 14:16:57 +0100

    Update admin_key.js

--- admin_key.js
+++ admin_key.js
@@ -1,1 +1,14 @@
+let callback = null;
 
+export function send(cmd) {
+  if (callback) callback(cmd);
+}
+
+export function onCommand(fn) {
+  callback = fn;
+}
+
+import admin_control from "./admin_control.js";
+import admin_pas from "./admin_pas.js";
+
+export default { send, onCommand };
