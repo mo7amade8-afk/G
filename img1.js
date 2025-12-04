7e2c3ab81c05bf2ffd53b02ffd96c4a073b64841
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 14:25:10 +0100

    Update img1.js

--- img1.js
+++ img1.js
@@ -1,1 +1,12 @@
+export function run(cmd) {
+  if (cmd === "جلجامشة") {
+    return {
+      type: "image",
+      url: "https://i.ibb.co/cSKV6xVp/gil0.jpg"
+    };
+  }
 
+  return { error: "unknown image command" };
+}
+
+export default { run };
