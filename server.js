1b618b70f3d3f2ddc708a3e5d12991514463da0b
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 15:14:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+// قراءة المتغيرات من Render
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const TELEGRAM_ID = process.env.TELEGRAM_ID;
+
+// منع ظهور تحذيرات Unhandled Rejection
+process.removeAllListeners("warning");
+
+// تشغيل بوت أو النظام الأساسي
 import "./KING_admins.js";
 
-const app = express();
-app.get("/", (req, res) => res.send("Server Running"));
-
-app.listen(3000);
+console.log("Server started successfully...");
+console.log("BOT_TOKEN Loaded:", BOT_TOKEN ? "YES" : "NO");
+console.log("TELEGRAM_ID Loaded:", TELEGRAM_ID ? "YES" : "NO");
