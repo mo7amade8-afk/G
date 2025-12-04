fc1eb4bf38d220a993bf39a0f74ea9ffe057b494
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 22:30:25 +0100

    Update admin_key.js

--- admin_key.js
+++ admin_key.js
@@ -1,14 +1,23 @@
-let callback = null;
+import adminControl from "./admin_control.js";
+import adminPas from "./admin_pas.js";
 
-export function send(cmd) {
-  if (callback) callback(cmd);
-}
+export default {
+  async handle(command) {
+    try {
+      // يرسل الأمر للتحكم الأساسي
+      if (adminControl[command]) {
+        return await adminControl[command]();
+      }
 
-export function onCommand(fn) {
-  callback = fn;
-}
+      // أو إذا كان موجود في admin_pas
+      if (adminPas[command]) {
+        return await adminPas[command]();
+      }
 
-import admin_control from "./admin_control.js";
-import admin_pas from "./admin_pas.js";
-
-export default { send, onCommand };
+      return null;
+    } catch (err) {
+      console.log("admin_key ERROR:", err);
+      return null;
+    }
+  }
+};
