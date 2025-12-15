76317bfdd3600892473bfa4bfa37d55361b0e86b
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-15 01:15:39 +0100

    Update admin_key.js

--- admin_key.js
+++ admin_key.js
@@ -1,18 +1,30 @@
 import adminControl from "./admin_control.js";
 import adminPass from "./admin_pas.js";
 
+
+async function handle(command) {
+  console.log("admin_key received:", command);
+
+  let res;
+
+  
+  res = await adminControl.handle(command);
+  if (res) return res;
+
+  
+  res = await adminPass.handle(command);
+  if (res) return res;
+
+  return null;
+}
+
+
+export async function send(command) {
+  return await handle(command);
+}
+
+
 export default {
-  async handle(command) {
-    console.log("admin_key received:", command);
-
-    let res;
-
-    res = await adminControl.handle(command);
-    if (res) return res;
-
-    res = await adminPass.handle(command);
-    if (res) return res;
-
-    return null;
-  }
+  handle,
+  send
 };
