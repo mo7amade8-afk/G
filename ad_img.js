85721d8649e2f8bd593fc43024f161a6879d9c96
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-31 22:52:55 +0100

    Update ad_img.js

--- ad_img.js
+++ ad_img.js
@@ -1,5 +1,10 @@
 import img1 from "./img_1.js";
+import img2 from "./img_2.js";
+import img3 from "./img_3.js";
 
-export default function adImg(bot, msg) {
-  img1(bot, msg);
+export default async function adImg(bot, msg) {
+
+  await img1(bot, msg);
+  await img2(bot, msg);
+  await img3(bot, msg);
 }
