a722c4ab60168b74fc39f5a53a170b9fde72a8b1
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-31 20:07:19 +0100

    Update king_admins.js

--- king_admins.js
+++ king_admins.js
@@ -2,9 +2,10 @@ import adText from "./ad_txt.js";
 import adImg from "./ad_img.js";
 import adVid from "./ad_vid.js";
 
-const ADMIN_ID = process.env.ADMIN_ID;
+const ADMIN_ID = process.env.ADMIN_ID; // ضع ايدي الأدمن هنا
 
 export default function KING(bot, msg) {
+
   if (msg.from.id != ADMIN_ID) return;
 
   if (msg.text) return adText(bot, msg);
