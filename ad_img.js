823174818076a666334a0fc8e5fffcf63de68634
mo7amade8-afk <mo7amade8@gmail.com>
2026-01-01 09:27:08 +0100

    Update ad_img.js

--- ad_img.js
+++ ad_img.js
@@ -1,8 +1,16 @@
 import img1 from "./img_1.js";
 import img2 from "./img_2.js";
 import img3 from "./img_3.js";
+import entrance from "./entrance.js";
+
+let entranceLoaded = false;
 
 export default async function adImg(bot, msg) {
+  // تسجيل حدث الترحيب مرة واحدة فقط
+  if (!entranceLoaded) {
+    entrance(bot);
+    entranceLoaded = true;
+  }
 
   await img1(bot, msg);
   await img2(bot, msg);
