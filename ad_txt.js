6b165e7d72717f78da051f77a0c51b0751198d2d
mo7amade8-afk <mo7amade8@gmail.com>
2026-01-06 10:11:38 +0100

    Update ad_txt.js

--- ad_txt.js
+++ ad_txt.js
@@ -1,9 +1,9 @@
-import txt1 from "./txt_1.js";
+import { textHandler } from "./txt_1.js";
 
 export default async function adText(bot, msg) {
   try {
     if (msg.text) {
-      await txt1(bot, msg);
+      await textHandler(bot, msg);
     }
   } catch (err) {
     console.error("❌ adText error:", err.message);
